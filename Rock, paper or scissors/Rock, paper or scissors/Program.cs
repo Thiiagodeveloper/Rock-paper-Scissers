d5d8af221ba8fbfd64e23760ca73cea45af34e15
Thiiagodeveloper <83225054+Thiiagodeveloper@users.noreply.github.com>
2023-02-19 12:00:27 -0500

    Eliminado líneas que no importan

--- Rock, paper or scissors/Rock, paper or scissors/Program.cs
+++ Rock, paper or scissors/Rock, paper or scissors/Program.cs
@@ -1,6 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 ///////////Juego de piedra papel y tijera////////////
 
 //Instancia para crear objeto de tipo Random
